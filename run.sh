d07b037162a0ddba2259cd3d55211b7638646312
root <sockyjam@126.com>
2020-11-24 09:50:18 +0800

    port 22 -> 10022

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run -d --name hadoop0 --hostname hadoop0 -P -p 50070:50070 -p 8088:8088 sockyjam/hadoop_jh:v1
-docker run -d --name hadoop1 --hostname hadoop1 -P sockyjam/hadoop_jh:v1
-docker run -d --name hadoop2 --hostname hadoop2 -P sockyjam/hadoop_jh:v1
+docker run -d --name hadoop0 --hostname hadoop0 -P -p 50070:50070 -p 8088:8088 -p 10022:22 sockyjam/hadoop_jh:v1
+docker run -d --name hadoop1 --hostname hadoop1 -P -p 10022:22 sockyjam/hadoop_jh:v1
+docker run -d --name hadoop2 --hostname hadoop2 -P -p 10022:22 sockyjam/hadoop_jh:v1
 
